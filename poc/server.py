b6cd8108208bac3b09a561378adaec88511cb05b
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2017-11-19 14:40:01 -0200

    POC example server

--- poc/server.py
+++ poc/server.py
@@ -7,21 +7,51 @@ export FLASK_APP=server.py && flask run
 from flask import Flask, request
 from flask_cors import CORS
 
+import json
+
 app = Flask(__name__)
 CORS(app)
 
+emails = {}
+urls = {}
+
 @app.route('/')
-def hello_world():
-    return 'Tracking app'
+def home():
+    return json.dumps(emails)
+
+@app.route('/detail/<guid>')
+def details(guid):
+    if guid in urls.keys():
+        return json.dumps(urls[guid])
+    
+    return 'Not found'
 
 @app.route('/track/email/<guid>', methods = ['POST'])
-@app.route('/track/url/<guid>', methods = ['POST'])
-def track(guid):
+def track_email(guid):
     
     if not request.json:
         abort(400)
     
-    print(guid, request.json)
+    email = request.json['email']
+    if email in emails.keys():
+        emails[email].append(guid)
+    else:
+        emails[email] = [guid]
+
+    print(emails)
     return '', 204
 
+@app.route('/track/url/<guid>', methods = ['POST'])
+def track_url(guid):
+
+    if not request.json:
+        abort(400)
+
+    data = request.json
+    if guid in urls.keys():
+        urls[guid].append(data)
+    else:
+        urls[guid] = [data]
     
+    print(urls)
+    return '', 204
